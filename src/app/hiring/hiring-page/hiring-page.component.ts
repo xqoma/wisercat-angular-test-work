186470df2391555bac1bfb1956a54f084bb5d139
Dmitry Grigoriev <dmitrycredo@gmail.com>
2022-02-24 16:09:55 +0200

    Implements form reset

--- src/app/hiring/hiring-page/hiring-page.component.ts
+++ src/app/hiring/hiring-page/hiring-page.component.ts
@@ -1,8 +1,41 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-hiring-page',
   templateUrl: './hiring-page.component.html',
   styleUrls: ['./hiring-page.component.scss'],
 })
-export class HiringPageComponent {}
+export class HiringPageComponent implements OnInit {
+  form!: FormGroup
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', Validators.required],
+      workExpMonths: ['', Validators.required],
+    })
+  }
+
+  onSubmit(): void {
+    console.log('Form is valid:', this.form.valid)
+    console.log(this.form.value)
+    // Here is a submit logic
+  }
+
+  onReset(): void {
+    this.resetForm()
+    // Show message method
+  }
+
+  resetForm(): void {
+    this.form.reset()
+  }
+}
--- src/app/hiring/hiring.module.ts
+++ src/app/hiring/hiring.module.ts
@@ -1,5 +1,6 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
+import {ReactiveFormsModule} from '@angular/forms'
 import {RouterModule, Routes} from '@angular/router'
 
 import {HiringPageComponent} from 'src/app/hiring/hiring-page/hiring-page.component'
@@ -12,7 +13,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
   declarations: [HiringPageComponent],
 })
 export class HiringModule {}
