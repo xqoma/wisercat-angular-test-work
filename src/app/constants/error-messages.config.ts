ba07071d4c42e3a6389a79c75161798421595fc4
Dmitry Grigoriev <dmitrycredo@gmail.com>
2022-02-25 01:10:27 +0200

    Implements display of field validation error

---
+++ src/app/constants/error-messages.config.ts
@@ -0,0 +1,4 @@
+export const ERROR_MESSAGES: Record<string, string> = {
+  required: 'This field is required.',
+  email: 'Invalid email format.',
+}
--- src/app/hiring/hiring-page/hiring-page.component.ts
+++ src/app/hiring/hiring-page/hiring-page.component.ts
@@ -1,5 +1,13 @@
 import {Component, OnInit} from '@angular/core'
-import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms'
+
+import {ERROR_MESSAGES} from 'src/app/constants/error-messages.config'
 
 @Component({
   selector: 'app-hiring-page',
@@ -15,13 +23,60 @@ export class HiringPageComponent implements OnInit {
     this.initializeForm()
   }
 
+  get name(): AbstractControl | null {
+    return this.form.get('name')
+  }
+
+  get surname(): AbstractControl | null {
+    return this.form.get('surname')
+  }
+
+  get email(): AbstractControl | null {
+    return this.form.get('email')
+  }
+
+  get workExpMonths(): AbstractControl | null {
+    return this.form.get('workExpMonths')
+  }
+
   initializeForm(): void {
-    this.form = this.fb.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      workExpMonths: ['', Validators.required],
+    this.form = this.fb.group(
+      {
+        name: ['', Validators.required],
+        surname: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        workExpMonths: ['', Validators.required],
+      },
+      {updateOn: 'blur'}
+    )
+  }
+
+  isErrorMsg(control: AbstractControl | null): boolean | null {
+    if (!control) return null
+
+    return control.invalid && control.touched
+  }
+
+  getErrorMsg(control: AbstractControl | null): string | null {
+    if (!control) return null
+
+    const errors: ValidationErrors | null = control.errors
+
+    if (!errors) return null
+
+    let errorMsg: string = ''
+    Object.keys(errors).forEach((keyErr) => {
+      if (keyErr in ERROR_MESSAGES) {
+        errorMsg += ERROR_MESSAGES[keyErr] + ' '
+      } else {
+        errorMsg += 'Unknown error: ' + keyErr
+      }
     })
+    return errorMsg
+  }
+
+  resetForm(): void {
+    this.form.reset()
   }
 
   onSubmit(): void {
@@ -33,9 +88,5 @@ export class HiringPageComponent implements OnInit {
   onReset(): void {
     this.resetForm()
     // Show message method
-  }
-
-  resetForm(): void {
-    this.form.reset()
   }
 }
